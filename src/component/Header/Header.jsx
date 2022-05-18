b81a0388f00811b2cf5ecb23c666badaaa550c5e
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-05-18 17:01:25 +0300

    header fix

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,32 @@
+import React from 'react';
+import svg from '../../image/svg/home.svg';
+
 import css from './Header.module.css';
 import { Nav } from "../indeх";
 // import Social from '../Social';
-
+import Modal from '../MenuMobile/Modal';
+import useModal from '../MenuMobile/useModal';
 function Header() {
+  const { isShowingModal, toggle, handleBackdropClick } = useModal();
   return (
     <div className={css.header__container}>
      
     <h1 className={css.header__title}><a href="/">Dmytro Pashchenko</a></h1>
     
     <div className={css.header__nav}><Nav/></div>
-    
+    <div className={css.header__modal}>
+    <svg className={css.header__menu}  onClick={() => toggle()} width="32" height="32">
+        <use href={`${svg}#icon-menu`}></use>
+      </svg>
+    {isShowingModal && (
+          <Modal
+            closeModal={toggle}
+            handleBackdropClick={handleBackdropClick}
+            
+            
+          />
+        )}
+    </div>
     
     
     </div>
