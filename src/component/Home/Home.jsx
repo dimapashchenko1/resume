1e6c0e8af87ff44bbc87816cb9e1cba98132b4dc
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-05-18 17:01:12 +0300

    home fix

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,28 @@
 import css from './Home.module.css';
 import { Nav, Social } from "../indeх";
-// import Social from '../Social';
+import React from 'react';
+import svg from '../../image/svg/home.svg';
+
+import Modal from '../MenuMobile/Modal';
+import useModal from '../MenuMobile/useModal';
 
 function Home() {
+  const { isShowingModal, toggle, handleBackdropClick } = useModal();
   return (
     <div className={css.home__container}>
+      <div className={css.home__modal}>
+    <svg className={css.home__menu}  onClick={() => toggle()} width="32" height="32">
+        <use href={`${svg}#icon-menu`}></use>
+      </svg>
+    {isShowingModal && (
+          <Modal
+            closeModal={toggle}
+            handleBackdropClick={handleBackdropClick}
+          />
+        )}
+    </div>
       <div className={css.home}>
+      
     <h1 className={css.home__title}>Dmytro Pashchenko</h1>
     <p className={css.home__text}>I am a Junior Full Stack developer from Kiev, Ukraine</p>
     <div className={css.home__nav}><Nav/></div>
